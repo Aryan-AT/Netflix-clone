582845457c348bb515e61b26a06fc8b654934695
Aryan-AT <84904373+Aryan-AT@users.noreply.github.com>
2026-01-28 14:18:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function playVideo(videoSrc){
+
 const main = document.getElementById("mainVideo");
+
 main.src = videoSrc;
 main.play();
-window.scrollTo({top:0, behavior:"smooth"});
+
+window.scrollTo({
+  top:0,
+  behavior:"smooth"
+});
+
 }
